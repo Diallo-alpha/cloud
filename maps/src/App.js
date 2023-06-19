099dc13ed98aeeddd3a5b6552ed2e924068d089a
Zapata-Diallo <alphaloppecity@gmail.com>
2023-06-19 12:17:15 +0000

    checkpoint cloud

---
+++ maps/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <iframe width="520" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=21000%20POINT%20E+(GomyCode)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href='https://maps-generator.com/'></a>
+
+    </div>
+  );
+}
+
+export default App;
